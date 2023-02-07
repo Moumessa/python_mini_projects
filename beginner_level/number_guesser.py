202d06e65d2e67b57ef5db33084feccc1c6c2694
Moumessa <46871421+Moumessa@users.noreply.github.com>
2023-02-07 22:18:10 +0100

    Update number_guesser.py

--- beginner_level/number_guesser.py
+++ beginner_level/number_guesser.py
@@ -13,17 +13,25 @@ else:
     quit()
 
 random_guess = random.randint(0,upper_limit)
+print('--random guess :', random_guess)
+
+number_of_guesses = 0
 
 while True:
     user_guess = input('Make a guess : ')
+    number_of_guesses += 1
+
     if user_guess.isdigit():
         user_guess = int(user_guess)
         if user_guess == random_guess :
-            print('You have got it !')
+            # print('You have got it !')
+            print('You hvae got it after ', number_of_guesses, ' guesses !')
+            break
         else:
-            print('You have got it wrong')
-        break
+            if user_guess > random_guess :
+                print('You are above the number !')
+            else:
+                print('You are below the number !')
     else:
-        print('Please make sure to inter a valid number')
-        continue
+        print('Please make sure to enter a valid number')
 
