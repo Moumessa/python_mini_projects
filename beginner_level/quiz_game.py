1cb912a490af4d0e116e7e6bd26d8ab06611fccb
Moumessa <46871421+Moumessa@users.noreply.github.com>
2023-02-07 20:23:12 +0100

    Update quiz_game.py

--- beginner_level/quiz_game.py
+++ beginner_level/quiz_game.py
@@ -7,20 +7,27 @@ if playing != 'yes':
 
 print("Okey ! Let's play !")
 
+score = 0
 answer = input('What does CPU stand for ?')
 if answer == 'Central Unit Processing':
     print('Correct')
+    score+=1
 else:
     print('Incorrect !')
 
 answer = input('What does GPU stand for ?')
 if answer == 'Graphical Unit Processing':
     print('Correct')
+    score+=1
 else:
     print('Incorrect !')
 
 answer = input('What does RAM stand for ?')
 if answer == 'Random Access Memory':
     print('Correct')
+    score+=1
 else:
     print('Incorrect !')
+
+
+print('You got', score, 'correct answers !')
