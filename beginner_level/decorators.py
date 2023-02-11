a94524cbacda984d57d0023455d4662f59172ade
Moumessa <46871421+Moumessa@users.noreply.github.com>
2023-02-11 21:49:00 +0100

    Update decorators.py

--- beginner_level/decorators.py
+++ beginner_level/decorators.py
@@ -1,27 +1,30 @@
-
 def my_func(f):
     """_summary_
 
     Args:
         f (_type_): _description_
     """
-    def wrapper():
+    def wrapper(*args, **kwargs):
         print('1')
-        f()
+        r = f(*args, **kwargs)
         print(2)
+
+        return r
 
     return wrapper
 
-def func_2():
-    print('Inside func 2')
+@my_func
+def func_2(x,y):
+    print('Inside func 2 :', x, y)
+    return x
 
+@my_func
 def func_3():
     print('Inside func 3')
 
-x = my_func(func_2)
-print(x)
-x()
+# func_2 = my_func(func_2)
+a = func_2(3,6)
+print('returned value of func_2 =', a)
 
-y = my_func(func_3)
-print(y)
-y()
+# func_3 = my_func(func_3)
+# func_3()
